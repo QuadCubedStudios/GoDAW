87a710eae34d7bea0329edaf7c5821558c205bb1
UltraAgent7 <57314648+UltraAgent7@users.noreply.github.com>
2021-02-02 21:23:46 +0530

    Remove script editor, add instrument selection system (#2)
    * Remove script editor, add instrument selection system
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Kuriakose <technohacker1995@gmail.com>

--- Main.gd
+++ Main.gd
@@ -1,8 +1,12 @@
 extends Control
 
 onready var player = $AudioStreamPlayer
+onready var instrument_tab = $VBoxContainer/EditorView/Instruments
 
 func _init():
+	pass
+
+func load_instruments():
 	var dir = Directory.new()
 	dir.open("res://Instruments")
 	dir.list_dir_begin(true, true)
@@ -12,13 +16,15 @@ func _init():
 		if dir.file_exists("./%s/instrument.gd" % instrument_name):
 			var instrument = load("%s/%s/instrument.gd" % [dir.get_current_dir(), instrument_name])
 			GoDAW.register_instrument(instrument_name, instrument.new())
+			instrument_tab.add_item(instrument_name, load("%s/%s/icon.png" % [dir.get_current_dir(), instrument_name]))
 		else:
 			push_warning("Instrument %s does not have an instrument.gd file" % instrument_name)
 
 		instrument_name = dir.get_next()
 
 func _ready():
+	load_instruments()
 	var osc = GoDAW.get_instrument("TripleOsc")
 	# Set stream and play
-	player.stream = osc.create_sample()
-	player.play()
+	#player.stream = osc.create_sample()
+	#player.play()
