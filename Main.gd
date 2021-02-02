2eafa96a52de82505d269f9e824128ef07c536bb
Kevin Kuriakose <technohacker1995@gmail.com>
2021-02-02 21:22:29 +0530

    Apply suggestions from code review

--- Main.gd
+++ Main.gd
@@ -1,12 +1,12 @@
 extends Control
 
 onready var player = $AudioStreamPlayer
-onready var instrumentTab= $VBoxContainer/EditorView/Instruments
+onready var instrument_tab = $VBoxContainer/EditorView/Instruments
 
 func _init():
 	pass
 
-func loadInstruments():
+func load_instruments():
 	var dir = Directory.new()
 	dir.open("res://Instruments")
 	dir.list_dir_begin(true, true)
@@ -16,14 +16,14 @@ func loadInstruments():
 		if dir.file_exists("./%s/instrument.gd" % instrument_name):
 			var instrument = load("%s/%s/instrument.gd" % [dir.get_current_dir(), instrument_name])
 			GoDAW.register_instrument(instrument_name, instrument.new())
-			instrumentTab.add_item(instrument_name, load("%s/%s/icon.png" % [dir.get_current_dir(), instrument_name]))
+			instrument_tab.add_item(instrument_name, load("%s/%s/icon.png" % [dir.get_current_dir(), instrument_name]))
 		else:
 			push_warning("Instrument %s does not have an instrument.gd file" % instrument_name)
 
 		instrument_name = dir.get_next()
 
 func _ready():
-	loadInstruments()
+	load_instruments()
 	var osc = GoDAW.get_instrument("TripleOsc")
 	# Set stream and play
 	#player.stream = osc.create_sample()
