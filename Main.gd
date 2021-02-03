075f9bed744a255441fe3fae71ddb544da6cc7c3
UltraAgent7 <svishu973@gmail.com>
2021-02-03 22:30:48 +0530

    A default icon is now displayed when an icon is not found in the instrument folder

--- Main.gd
+++ Main.gd
@@ -17,6 +17,11 @@ func load_instruments():
 			var instrument = load("%s/%s/instrument.gd" % [dir.get_current_dir(), instrument_name])
 			GoDAW.register_instrument(instrument_name, instrument.new())
 			instrument_tab.add_item(instrument_name, load("%s/%s/icon.png" % [dir.get_current_dir(), instrument_name]))
+			if dir.file_exists("./%s/icon.png" % instrument_name):
+				instrument_tab.add_item(instrument_name, load("%s/%s/icon.png" % [dir.get_current_dir(), instrument_name]))
+			else:
+				instrument_tab.add_item(instrument_name, load("res://util/defaultIcon.png"))
+				
 		else:
 			push_warning("Instrument %s does not have an instrument.gd file" % instrument_name)
 
