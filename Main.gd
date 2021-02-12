6fc2d1cce3544688f1ee57cdfe3799029f0c580e
UltraAgent7 <>
2021-02-12 19:32:54 +0530

    Revamped the UI

--- Main.gd
+++ Main.gd
@@ -32,7 +32,7 @@ func _ready():
 	var osc = GoDAW.get_instrument("TripleOsc")
 	var wave = Wave.new(osc.create_sample())
 	wave.position += Vector2(10, 200)
-	add_child(wave)
+	#add_child(wave)
 	# Set stream and play
 	player.stream = osc.create_sample()
 	player.pitch_scale = 1.0
