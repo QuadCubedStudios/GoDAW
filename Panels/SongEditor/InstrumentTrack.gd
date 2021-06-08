fa7d620309317ca26fde44c39a9dccd17a84f198
Abhinav Kuruvila Joseph <abhinav.kuruvila.joseph@gmail.com>
2021-06-08 10:49:36 +0530

    Piano ROll open with double click

--- Panels/SongEditor/InstrumentTrack.gd
+++ Panels/SongEditor/InstrumentTrack.gd
@@ -6,3 +6,9 @@ func set_instrument(icon: Texture, instrument_name: String) -> void:
 	instrument_toggle.icon = icon
 	instrument_toggle.text = (instrument_name if instrument_name.length() <= 20
 								else instrument_name.substr(0, 17) + "...")
+
+
+func _on_Segments_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.doubleclick:
+			$Panel.visible = !$Panel.visible
