3575e65202d7f0a4152bc9981faa06623d739a83
Abhinav Kuruvila Joseph <abhinav.kuruvila.joseph@gmail.com>
2021-06-09 12:09:13 +0530

    scroll & delete tracks

--- Panels/SongEditor/InstrumentTrack.gd
+++ Panels/SongEditor/InstrumentTrack.gd
@@ -1,14 +1,38 @@
 extends Control
 
+export var scroll = 0 setget set_scroll
+
+var instrument_toggle : ToolButton
+
+onready var segments = $HBoxContainer/ScrollView/Segments
+onready var delete_confirm = $DeleteConfirm
+
+func set_scroll(val):
+	if val >= 0:
+		segments.set_position(Vector2(-val*10, 0))
+	scroll = val
 
 func set_instrument(icon: Texture, instrument_name: String) -> void:
-	var instrument_toggle = $HBoxContainer/InstrumentToggle
+	instrument_toggle = $HBoxContainer/InstrumentToggle
 	instrument_toggle.icon = icon
 	instrument_toggle.text = (instrument_name if instrument_name.length() <= 20
 								else instrument_name.substr(0, 17) + "...")
 
+func _ready():
+	for i in range(100):
+		segments.add_child(Button.new())
 
 func _on_Segments_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.doubleclick:
 			$Panel.visible = !$Panel.visible
+
+func _on_InstrumentToggle_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 2 && event.pressed:
+			delete_confirm.dialog_text = (
+				"Are you sure you want to delete track " + instrument_toggle.text)
+			delete_confirm.popup_centered()
+
+func _on_DeleteConfirm_confirmed():
+	queue_free()
--- Panels/SongEditor/SongEditor.gd
+++ Panels/SongEditor/SongEditor.gd
@@ -2,13 +2,20 @@ extends VBoxContainer
 
 var InstrumentTrack = preload("./InstrumentTrack.tscn")
 
+onready var tracks = $ScrollContainer/Tracks
+
 # Takes a Button since it conveniently sends an icon and message
 func add_track(instrument: Button):
 	var inst = InstrumentTrack.instance()
 	inst.set_instrument(instrument.icon, instrument.text)
-	$ScrollContainer/Tracks.add_child(inst)
+	tracks.add_child(inst)
 #	inst.size_flags_horizontal |= SIZE_FILL
 #	inst.rect_min_size.x = 192
 
 func _on_InstrumentsPanel_instrument_chosen(instrument) -> void:
 	add_track(instrument)
+
+
+func _on_HScrollBar_value_changed(value):
+	for track in tracks.get_children():
+		track.scroll = value
