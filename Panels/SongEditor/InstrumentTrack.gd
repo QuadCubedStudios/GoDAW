bad3336df771f3e6706ae8572c5e6eeb7d9ffd64
Abhinav Kuruvila Joseph <abhinav.kuruvila.joseph@gmail.com>
2021-06-09 21:09:25 +0530

    Segments

--- Panels/SongEditor/InstrumentTrack.gd
+++ Panels/SongEditor/InstrumentTrack.gd
@@ -4,6 +4,11 @@ export var scroll = 0 setget set_scroll
 
 var instrument_toggle : ToolButton
 
+# styles
+var style_1 = preload("res://Theme/Default/SongButton1.tres")
+var style_2 = preload("res://Theme/Default/SongButton2.tres")
+var style_pressed = preload("res://Theme/Default/SongButtonPressed.tres")
+
 onready var segments = $HBoxContainer/ScrollView/Segments
 onready var delete_confirm = $DeleteConfirm
 
@@ -20,12 +25,24 @@ func set_instrument(icon: Texture, instrument_name: String) -> void:
 
 func _ready():
 	for i in range(100):
-		segments.add_child(Button.new())
+		var segment = Button.new()
+		segment.rect_min_size.x = 20
+		segment.focus_mode = Control.FOCUS_NONE
+		segment.add_stylebox_override("disabled", style_pressed)
+		segment.add_stylebox_override("normal", style_1 if i % 2 == 0 else style_2)
+		segment.add_stylebox_override("hover", style_1 if i % 2 == 0 else style_2)
+		segment.connect("gui_input", self, "segment_input", [segment])
+		segments.add_child(segment)
 
-func _on_Segments_gui_input(event):
+func segment_input(event, segment: Button):
 	if event is InputEventMouseButton:
-		if event.doubleclick:
-			$Panel.visible = !$Panel.visible
+		if event.pressed:
+			if event.button_index == 1:
+				segment.disabled = true
+			if event.doubleclick :
+				$Panel.visible = !$Panel.visible
+			if event.button_index == 3:
+				segment.disabled = false
 
 func _on_InstrumentToggle_gui_input(event):
 	if event is InputEventMouseButton:
