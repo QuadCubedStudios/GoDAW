45685066f9bc984782161c2b2250b9e2b0dc6507
Technohacker <technohacker1995@gmail.com>
2021-07-31 16:51:03 +0530

    Add Instrument loading dialog

--- Main.gd
+++ Main.gd
@@ -1,25 +1,42 @@
 extends Control
 
+onready var loading_dialog: WindowDialog = $DialogBoxes/LoadingDialog
+onready var loading_dialog_progress: ProgressBar = $DialogBoxes/LoadingDialog/VBoxContainer/ProgressBar
+
 func load_instruments():
 	var dir = Directory.new()
 	dir.open("res://Instruments")
 	dir.list_dir_begin(true, true)
 
+	var instruments = []
+
 	var instrument_name = dir.get_next()
 	while instrument_name != "":
 		if dir.file_exists("./%s/Instrument.tscn" % instrument_name):
-			var instrument: PackedScene = load("%s/%s/Instrument.tscn" % [dir.get_current_dir(), instrument_name])
-			GoDAW.register_instrument(instrument_name, instrument)
+			instruments.append(instrument_name)
 		else:
 			push_warning("Instrument %s does not have an Instrument.tscn file" % instrument_name)
 
 		instrument_name = dir.get_next()
 
-func _init():
-	load_instruments()
+	loading_dialog_progress.max_value = instruments.size()
+
+	for name in instruments:
+		$DialogBoxes/LoadingDialog/VBoxContainer/Label.text = name
+
+		yield(get_tree(), "idle_frame")
+		var instrument: PackedScene = load("%s/%s/Instrument.tscn" % [dir.get_current_dir(), name])
+		GoDAW.register_instrument(name, instrument)
+		loading_dialog_progress.value += 1
+
+	loading_dialog.hide()
 
 # This function is solely testing code
 func _ready():
+	loading_dialog.popup()
+
+	yield(load_instruments(), "completed")
+
 	var song = SongSequence.new()
 	var track = Track.new()
 	track.instrument = "DTMF"
