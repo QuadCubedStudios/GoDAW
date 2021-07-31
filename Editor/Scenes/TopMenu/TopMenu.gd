41afcec757a744eedd690f0259b9ab6ac8658a4d
Abhinav Kuruvila Joseph <abhinav.kuruvila.joseph@gmail.com>
2021-07-31 12:30:40 +0530

    Basic Export

--- Editor/Scenes/TopMenu/TopMenu.gd
+++ Editor/Scenes/TopMenu/TopMenu.gd
@@ -3,7 +3,7 @@ extends HBoxContainer
 onready var menus = {
 	"File": {
 		"node": $FileMenu,
-		"elements": { "Open": "_open", "Save": "", "Save as...": "", "New": "", "Separator": "", "Quit": "" }
+		"elements": { "New": "", "Open": "_open", "Save": "", "Save as...": "", "Export": "_export" ,"Separator": "", "Quit": "" }
 	},
 	"Edit": {
 		"node": $EditMenu,
@@ -40,6 +40,20 @@ func on_item_pressed(id, menu):
 
 func _open():
 	print("Opened")
+
+func _export():
+	var sequencer = get_parent().get_node("Application/Main/SongEditor/Sequencer")
+	var recorder = AudioEffectRecord.new()
+	AudioServer.add_bus_effect(0, recorder)
+	recorder.set_recording_active(true)
+	sequencer.connect("playback_finished", self, "_export_finished", [recorder])
+	sequencer.play()
+	pass
+
+func _export_finished(recorder: AudioEffectRecord):
+	recorder.set_recording_active(false)
+	var recording = recorder.get_recording()
+	recording.save_to_wav("/tmp/GoDAW_Export.wav")
 
 func init_menu(menu, items):
 	for e in items:
