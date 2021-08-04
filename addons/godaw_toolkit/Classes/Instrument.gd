16ebe513cdd4596b923a86b1b874987b56fbe434
AbhinavKDev <62714538+abhinav3967@users.noreply.github.com>
2021-08-04 12:19:24 +0530

    Create new audio buses for each instrument (#32)

--- addons/godaw_toolkit/Classes/Instrument.gd
+++ addons/godaw_toolkit/Classes/Instrument.gd
@@ -2,10 +2,15 @@ extends Node
 
 class_name Instrument
 
+var bus_idx: int
 var instrument_name
 
 func _init(instrument_name: String):
 	self.instrument_name = instrument_name
+	
+	# Each instrument need to output audio to this bus
+	self.bus_idx = AudioServer.bus_count
+	AudioServer.add_bus(self.bus_idx)
 
 func play_note(note):
 	pass
