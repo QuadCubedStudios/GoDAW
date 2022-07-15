8a7db13f9365796d84cf4cbc7852606d4f803ff9
Isotoxal <62714538+IsotoxalDev@users.noreply.github.com>
2022-07-15 21:31:48 +0530

    Gridworks 😎

--- Editor/Scenes/Dialogues/TrackEditor/Grid.gd
+++ Editor/Scenes/Dialogues/TrackEditor/Grid.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+var size: int = 25
+var thickness = 2
+onready var track_editor = $"../../../"
+
 func _draw():
-	pass
+	draw_line(Vector2(0, 0), Vector2(0, get_parent().rect_size.y), Color("#9191aa"), thickness)
+	for y in get_parent().rect_size.y/size:
+		if y*size-track_editor.scroll.scroll_vertical % size > 0:
+			draw_line(Vector2(0, y*size-track_editor.scroll.scroll_vertical % size), Vector2(get_parent().rect_size.x, y*size-track_editor.scroll.scroll_vertical % size), Color("#9191aa"), thickness)
+	for x in get_parent().rect_size.x/size - track_editor.name_scroll.rect_size.x/size:
+		if x*size+track_editor.name_scroll.rect_size.x-track_editor.scroll.scroll_horizontal % size > track_editor.name_scroll.rect_size.x:
+			draw_line(Vector2(x*size+track_editor.name_scroll.rect_size.x-track_editor.scroll.scroll_horizontal % size, 0), Vector2(x*size+track_editor.name_scroll.rect_size.x-track_editor.scroll.scroll_horizontal % size, get_parent().rect_size.y), Color("#9191aa"), thickness)
--- Editor/Scenes/Dialogues/TrackEditor/TrackEditor.gd
+++ Editor/Scenes/Dialogues/TrackEditor/TrackEditor.gd
@@ -1,10 +1,11 @@
 extends WindowDialog
 
 onready var settings  = $HSplit/Settings
-onready var name_scroll = $HSplit/HBoxContainer/NameScroll
+onready var name_scroll = $HSplit/HBoxContainer/VBoxContainer/NameScroll
 onready var scroll = $HSplit/HBoxContainer/Scroll
-onready var note_names = $HSplit/HBoxContainer/NameScroll/NoteName
+onready var note_names = $HSplit/HBoxContainer/VBoxContainer/NameScroll/NoteName
 onready var notes = $HSplit/HBoxContainer/Scroll/Notes
+onready var grid = $HSplit/HBoxContainer/Grid
 
 var scrolling_on_editor = false
 
@@ -15,39 +16,43 @@ func _ready():
 	
 	name_scroll.get_v_scrollbar().connect("value_changed", self, "name_scroll_changed")
 	scroll.get_v_scrollbar().connect("value_changed", self, "scroll_changed")
+	scroll.get_h_scrollbar().connect("value_changed", self, "h_scroll")
 
 func _popup(instrument):
 	window_title = "Edit %s" % instrument.instrument_name
 	var s
-	for i in range(50):
+	for i in range(80):
 		var b = Button.new()
 		b.disabled
 		b.text = "NOTE"
 		note_names.add_child(b)
 		s = b.rect_size
-		print(s)
 	var nmg = Image.new()
-	nmg.create(200, 50, false, Image.FORMAT_RGBA8)
+	nmg.create(100, 80, false, Image.FORMAT_RGBA8)
 	nmg.fill(Color.transparent)
 	nmg.lock()
-	for i in 25:
-		nmg.set_pixel(0, i+i*1, Color.white)
+	for i in 40:
+		nmg.set_pixel(i, i+i*1, Color.white)
 	nmg.unlock()
 	var image = ImageTexture.new()
 	image.create_from_image(nmg, 1)
 	var t = TextureRect.new()
 	t.expand = true
-	t.rect_min_size = Vector2(200*s.y+1, 50*s.y+1)
+	t.rect_min_size = Vector2(100*s.y, 80*s.y)
 	t.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
 	t.texture = image
 	notes.add_child(t)
-	popup()
+	popup_centered()
 
 func name_scroll_changed(_n):
 	scroll.scroll_vertical = name_scroll.scroll_vertical
+	grid.update()
 	
 func scroll_changed(_n):
 	name_scroll.scroll_vertical = scroll.scroll_vertical
+	grid.update()
+
+func h_scroll(_n): grid.update()
 
 func _on_NoteName_resized():
 	scroll.rect_size = name_scroll.rect_size
